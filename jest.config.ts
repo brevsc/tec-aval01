3df5849cc2ddbbe3e31d5cb8b360e1ab95fda4c4
Dymas <dymassousa@gmail.com>
2025-01-08 14:08:23 -0300

    chore: jest configuration to work with typescript

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  verbose: true,
+  coverageDirectory: './coverage',
+  coverageProvider: 'babel',
+  moduleNameMapper: {
+    '@/test/(.+)': '<rootDir>/test/$1',
+    '@/(.+)': '<rootDir>/src/$1',
+  },
+  roots: ['<rootDir>/src', '<rootDir>/test'],
+  testEnvironment: 'jest-environment-node',
+  transform: {
+    '\\.ts$': 'ts-jest',
+  },
+}
+
+export default config
