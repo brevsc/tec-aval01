9216362fcfa65cc2cb6f7f354cf4ebe1903df58f
Dymas <dymassousa@gmail.com>
2025-01-08 14:10:11 -0300

    feat: add Account entitty class

---
+++ src/account/signup/entities/account.ts
@@ -0,0 +1,13 @@
+export class Account {
+  constructor(
+    readonly account_id: string,
+    readonly name: string,
+    readonly email: string,
+    readonly cpf: string,
+    readonly car_plate: string,
+    readonly is_passenger: boolean,
+    readonly is_driver: boolean,
+    readonly password: string,
+    readonly password_algorithm: string
+  ) {}
+}
